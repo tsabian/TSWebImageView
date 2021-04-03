bca82994e41ec793088b2175aa3386f8948d562b
Tiago A. de Oliveira <tsabian@hotmail.com>
2021-04-03 02:13:27 -0300

    Setup project

---
+++ TSWebImageView/Core/TSWebImageView.swift
@@ -0,0 +1,50 @@
+//
+//  TSWebImageView.swift
+//  TSWebImageView
+//
+//  Created by Tiago Oliveira on 03/04/21.
+//
+
+import UIKit
+
+public final class TSWebImageView: UIImageView {
+    
+    // MARK: - Cache
+    fileprivate static var ramImageCache = NSCache<NSURL, UIImage>()
+    static subscript(url: URL) -> UIImage? {
+        get {
+            guard let currentURL = NSURL(string: url.absoluteString) else {
+                return nil
+            }
+            return ramImageCache.object(forKey: currentURL)
+        }
+        set {
+            guard let currentURL = NSURL(string: url.absoluteString) else {
+                return
+            }
+            if let newImage = newValue {
+                ramImageCache.setObject(newImage, forKey: currentURL)
+            } else {
+                ramImageCache.removeObject(forKey: currentURL)
+            }
+        }
+    }
+    
+    // MARK: - Inspectable
+    @IBInspectable
+    var webURLstring: String? {
+        didSet {
+            guard let webURLstring = webURLstring, !webURLstring.isEmpty, let url = URL(string: webURLstring) else {
+                return
+            }
+            webURL = url
+        }
+    }
+    
+    // MARK: - properties
+    private var webURL: URL?
+    
+
+    
+    
+}
